d3a71307e5f3b581d6ca251176c4faa5597b0a14
msvajgl <msvajgl@gmail.com>
2025-02-20 08:13:10 +0100

    added transformation file

---
+++ transformation.py
@@ -0,0 +1,73 @@
+import xml.etree.cElementTree as ET
+import json
+from datetime import datetime
+
+def get_text(element):
+    return element.text if element is not None else ''
+
+def parse_date(date_str):
+    try:
+        return datetime.strptime(date_str, "%Y%m%d").strftime("%Y-%m-%dT00:00:00")
+    except (ValueError, TypeError):
+        return ''
+
+sample_string = input
+sample_string_bytes = sample_string.encode("UTF-8")
+root = ET.fromstring(sample_string_bytes)
+namespace = {"ns": "http://schema.infor.com/InforOAGIS/2"}
+
+fields = [
+    "Company", "Division", "ItemNumber", "ProductName", "ItemDescription", "Status", "ItemType",
+    "ItemGroup", "MakeBuyCode", "AccessControlObject", "BusinessArea", "ProductGroup", "BasicUoM",
+    "AlternativeUoM", "HierachyLVL1", "HierachyLVL1Label", "HierachyLVL2", "HierachyLVL2Label",
+    "HierachyLVL3", "HierachyLVL3Label", "Code", "ShelfLife", "Supplier", "SupplierName", "NetWeight",
+    "GrossWeight", "Lenght", "Width", "Height", "ItemResponsibleID", "ItemResponsibleName",
+    "RegistrationDate", "LastChangeDate", "ChangeByID", "ChangeByName"
+]
+
+# Extracting and cleaning values
+data = {field: get_text(root.find(f".//ns:{field}", namespace)) for field in fields}
+
+data["RegistrationDate"] = parse_date(data["RegistrationDate"])
+data["LastChangeDate"] = parse_date(data["LastChangeDate"])
+
+# Special block for repeated elements
+aliases = [
+    f"{get_text(alias.find('ns:AliasQualifier', namespace))}:{get_text(alias.find('ns:AliasNumber', namespace))}"
+    for alias in root.findall(".//ns:Alias", namespace)
+    if alias.find("ns:AliasQualifier", namespace) is not None and alias.find("ns:AliasNumber", namespace) is not None
+]
+data["Aliases"] = ", ".join(aliases)
+
+# Mapping to output structure
+output_data = {
+    "m3_id": data["ItemNumber"],
+    "active_status": data["Status"],
+    "name": data["ProductName"],
+    "internal_comp_num_c": data["Company"],
+    "item_type_c": data["ItemType"],
+    "item_group_c": data["ItemGroup"],
+    "make_buy_c": data["MakeBuyCode"],
+    "acc_control_c": data["AccessControlObject"],
+    "area_c": data["BusinessArea"],
+    "product_group_c": data["ProductGroup"],
+    "unit_c": data["BasicUoM"],
+    "alt_unit_c": data["AlternativeUoM"],
+    "category_lvl1_id": data["HierachyLVL1"],
+    "category_lvl2_id": data["HierachyLVL2"],
+    "category_id": data["HierachyLVL3"],
+    "expiration_time_c": data["ShelfLife"],
+    "manufacturer_code_c": data["Supplier"],
+    "manufacturer_name_c": data["SupplierName"],
+    "part_num_c": data["Aliases"],
+    "weight": data["NetWeight"],
+    "weight_brutto_c": data["GrossWeight"],
+    "lenght_c": data["Lenght"],
+    "width_c": data["Width"],
+    "height_c": data["Height"],
+    "assigned_user_id": data["ItemResponsibleID"],
+    "description": data["ItemDescription"],
+    "date_entered": data["RegistrationDate"],
+    "date_modified": data["LastChangeDate"],
+    "modified_user_id": data["ChangeByID"]
+}
